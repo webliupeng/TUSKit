b5ea2feca98ae081f10d20edbbf0332b120fb5e5
Mark R. Masterson <mrobertmasterson@gmail.com>
2016-10-22 22:29:05 -0700

    Initial Target Added

---
+++ Example/macOS-Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS-Example
+//
+//  Created by Mark Masterson on 10/22/16.
+//  Copyright © 2016 Michael Avila. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/macOS-Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS-Example
+//
+//  Created by Mark Masterson on 10/22/16.
+//  Copyright © 2016 Michael Avila. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
